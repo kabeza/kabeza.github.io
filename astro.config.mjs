da4a84be5209c49ada618c0664f46599d30f05d5
Kike Becerra <kabeza@gmail.com>
2026-04-17 18:53:14 -0300

    hacerlo relativo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import relativeLinks from 'astro-relative-links';
 
 export default defineConfig({
+  integrations: [relativeLinks()],
   i18n: {
     defaultLocale: 'en',
     locales: ['en', 'es'],
